f83d98845baba58705b565f57ef68ef603a17d32
ropoko <rodrigostramantinoli@gmail.com>
2022-12-13 20:00:07 -0300

    feat: added eslint

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import { errorMiddleware } from "./middlewares/errorMiddleware";
-import { router } from "./routes";
+import express from 'express';
+// import { errorMiddleware } from "./middlewares/errorMiddleware";
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
-app.use(errorMiddleware);
+// app.use(errorMiddleware);
 
 export { app }
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router } from 'express';
 // import { authMiddleware } from "./middlewares/AuthenticationMiddleware";
 
 const router = Router()
